36472948191d97e03c30bda3986131e8d596fdae
Kyomin Ku <moeday1010@gmail.com>
2023-12-05 10:18:23 +0900

    Linked List - Push

--- linked-list.js
+++ linked-list.js
@@ -12,6 +12,22 @@ class LinkedList {
     this.tail = newNode;
     this.length = 1;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const linkedList = new LinkedList(4);
+linkedList.push(7);
+
+console.log(linkedList);
