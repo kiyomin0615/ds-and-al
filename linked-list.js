3b8bac6b3ccb3061d65e1740f3659c7f9a01e30e
Kyomin Ku <moeday1010@gmail.com>
2023-12-07 11:26:28 +0900

    Linked List - Pop

--- linked-list.js
+++ linked-list.js
@@ -25,9 +25,34 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (this.length === 0) {
+      return;
+    }
+
+    let temp = this.head;
+    let pre = this.head;
+
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return temp;
+  }
 }
 
 const linkedList = new LinkedList(4);
 linkedList.push(7);
-
+console.log(linkedList.pop());
 console.log(linkedList);
