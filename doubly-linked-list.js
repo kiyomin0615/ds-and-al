5ed7a14bce35156c2486de7d78524a934cc3cc37
Kyomin Ku <moeday1010@gmail.com>
2023-12-12 13:44:53 +0900

    Doubly Linked List - Pop

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -30,8 +30,30 @@ class DoublyLinkedList {
 
     return this;
   }
+
+  pop() {
+    if (this.length === 0) {
+      return null;
+    }
+
+    let temp = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+    
+    this.length--;
+
+    return temp;
+  }
 }
 
 const doublyLinkedList = new DoublyLinkedList(10);
 doublyLinkedList.push(32);
+console.log(doublyLinkedList.pop());
 console.log(doublyLinkedList);
