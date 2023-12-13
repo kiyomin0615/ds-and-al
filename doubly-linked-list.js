b9ac6d8a5f5a44d49a058c7f44bac8bc226b75c2
Kyomin Ku <moeday1010@gmail.com>
2023-12-13 09:17:31 +0900

    Doubly Linked List - Shift

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -68,10 +68,32 @@ class DoublyLinkedList {
 
     return this;
   }
+
+  shift() {
+    if (this.length === 0) {
+      return null;
+    }
+
+    let temp = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+
+    this.length--;
+
+    return temp;
+  }
 }
 
 const doublyLinkedList = new DoublyLinkedList(10);
 doublyLinkedList.push(32);
 console.log(doublyLinkedList.pop());
 doublyLinkedList.unshift(1);
+console.log(doublyLinkedList.shift());
 console.log(doublyLinkedList);
