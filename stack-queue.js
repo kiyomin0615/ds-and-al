54f4eb7f268a48abfed2f64876ca867762ab7326
Kyomin Ku <moeday1010@gmail.com>
2023-12-13 11:43:04 +0900

    Queue - Constructor

--- stack-queue.js
+++ stack-queue.js
@@ -1,5 +1,7 @@
 // Array로 Stack을 구현하는 것은 너무나도 간단하다
 // Linked List로 Stack을 구현할 때는, Linked List의 Head를 Stack의 Top으로 설정하는 것이 좋다
+// Array로 Queue를 구현하는 것보다 Linked List로 구현하는 것이 더 빠르다
+// Linked List로 Queue를 구현할 때는, Linked List의 Head를 Queue의 First로, Tail을 Last로 설정하는 것이 좋다
 class Node {
   constructor(value) {
     this.value = value;
@@ -24,7 +26,7 @@ class Stack {
       newNode.next = this.top;
       this.top = newNode;
     }
-
+    
     this.height++;
 
     return this;
@@ -46,6 +48,15 @@ class Stack {
   }
 }
 
+class Queue {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+}
+
 let stack = new Stack(11);
 stack.push(23);
 stack.push(9);
@@ -53,3 +64,6 @@ stack.push(20);
 stack.pop();
 stack.pop();
 console.log(stack);
+
+let queue = new Queue(4);
+console.log(queue);
