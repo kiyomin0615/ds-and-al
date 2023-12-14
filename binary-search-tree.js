ce42f8f9b66a4a2b1aa2dc85c0683526f45431c1
Kyomin Ku <moeday1010@gmail.com>
2023-12-14 09:39:38 +0900

    Binary Search Tree - Insert (Pseudo)

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -3,6 +3,7 @@ class Node {
     this.value = value;
     this.left = null;
     this.right = null;
+    // this.count = 1;
   }
 }
 
@@ -10,6 +11,15 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  // Insert
+  // create newNode
+  // if root === null, then root = newNode
+  // let temp = this.root
+  // while loop
+    // if newNode === temp, then return null
+    // if < left, else > right
+    // if null insert newNode, else move to next
 }
 
 let bst = new BinarySearchTree();
