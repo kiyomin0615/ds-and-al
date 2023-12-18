ea85e0eff126b5d8acfb0ed2052023c4dc131249
Kyomin Ku <moeday1010@gmail.com>
2023-12-18 13:35:04 +0900

    How to compare two arrays - O(n)

--- compare-array.js
+++ compare-array.js
@@ -8,7 +8,23 @@ function compareTwoArray1(arr1, arr2) {
   return false;
 }
 
+// O(n)
+function compareTwoArray2(arr1, arr2) {
+  let obj = {};
+
+  for (let i = 0; i < arr1.length; i++) {
+    obj[arr1[i]] = true;
+  }
+
+  for (let j = 0; j < arr2.length; j++) {
+    if (obj[arr2[j]]) return true;
+  }
+
+  return false;
+}
+
 let array1 = [1, 3, 5];
 let array2 = [2, 4, 5];
 
 console.log(compareTwoArray1(array1, array2));
+console.log(compareTwoArray2(array1, array2));
