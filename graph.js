af8f0195d6211f10500c574d170303efa649b5fe
Kyomin Ku <moeday1010@gmail.com>
2023-12-21 11:43:03 +0900

    Graph - AddEdge

--- graph.js
+++ graph.js
@@ -14,8 +14,20 @@ class Graph {
 
     return false;
   }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return true;
+    }
+
+    return false;
+  }
 }
 
 let graph = new Graph();
 graph.addVertex("A");
+graph.addVertex("B");
+graph.addEdge("A", "B");
 console.log(graph);
