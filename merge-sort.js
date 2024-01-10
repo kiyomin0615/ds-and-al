419b9413e86c1cf45408337ad805ed63dbb13afd
Kyomin Ku <moeday1010@gmail.com>
2024-01-10 17:24:43 +0900

    Sort - Merge Sort (2)

--- merge-sort.js
+++ merge-sort.js
@@ -25,4 +25,16 @@ function merge(array1, array2) {
   return combined;
 }
 
-console.log(merge([1, 3, 7, 8], [2, 4, 5, 6]));
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+
+  let mid = Math.floor(array.length / 2);
+  let left = array.slice(0, mid);
+  let right = array.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+console.log(mergeSort([2, 4, 6, 5, 1, 3]));
