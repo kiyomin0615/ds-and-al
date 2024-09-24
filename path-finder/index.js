c92c6044c783d7b302e9963eeb131ec46ca49771
kiyomin0615 <moeday1010@gmail.com>
2024-09-24 13:43:29 +0900

    start to create 'path finder on maze' with BFS and Dijkstra

---
+++ path-finder/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const startButtonEl = document.getElementById("start-button");
+  startButtonEl.addEventListener("click", startGame);
+});
+
+function startGame() {
+  // ...
+}
